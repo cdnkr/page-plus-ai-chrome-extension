2f2a04fa762995c7096fe907cf5d1c4574073c8b
Chad Danker <chad@forgood.co.za>
2025-10-15 00:50:47 +0200

    corrects padding on download api model button

--- popover.js
+++ popover.js
@@ -815,7 +815,7 @@ function getContextHTML({
 function getModelDownloadButtonHTML({
     dataKey,
 }) {
-    return `<button style="padding: 2px 8px;" class="action-btn" data-action="download" data-key="${dataKey}"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg></button>`;
+    return `<button class="action-btn" data-action="download" data-key="${dataKey}"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg></button>`;
 }
 
 function getCircularDownloadProgressHTML({
